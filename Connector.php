e0464e8f0ff2ddae2d797e40f9b732dd13df1696
dmitriy.luzhansky <dmitriy.luzhansky@gmail.com>
2021-08-05 21:36:59 +0400

    end update + get contacts

--- Connector.php
+++ Connector.php
@@ -7,8 +7,7 @@ class Connector
 {
 
     private static $connect_url = 'http://api.oppo-gdu.ru/'; //точка входа
-//    private static $sections = ['news', 'photo-albums']; // категории для списков и обновления данных
-    private static $sections = ['news'];
+    private static $sections = ['news', 'photo-albums']; // категории для списков и обновления данных
 
 
     // для получения данных траницы
@@ -33,8 +32,22 @@ class Connector
 
     }
 
-    //обновление данных в БД
-    public static function reload_section_data()
+    //для вызова обновления всей информаци
+    public static function update_all_information(){
+
+        $result = [];
+
+        $result[] = self::reload_section_data();
+        $result[] = self::reload_news_content();
+        $result[] = self::reload_photo();
+
+        return $result;
+
+    }
+
+
+    //обновление данных категорий в БД
+    private static function reload_section_data()
     {
         //для результатов обновления
         $status = [];
@@ -48,7 +61,7 @@ class Connector
 
                 //тут получаем нужные поля для загрузки из БД
                 $need_section_items = [];
-                $db_fields = DB::fast_query('DESCRIBE ' . $section);
+                $db_fields = DB::fast_query('DESCRIBE `' . $section . '`');
 
                 foreach ($db_fields as $db_field) {
 
@@ -71,39 +84,27 @@ class Connector
 
                     }
 
-                    //проверяем есть ли такая запись в базе
-                    $in_base = DB::fast_query('SELECT * FROM ' . $section . ' WHERE id =' . $section_item['id']);
+                    $pre_query = '';
 
-                    //или добовляем или обновляем
-                    if ($in_base) {
+                    foreach ($prepare_data_arr as $name => $val) {
 
-                        unset($prepare_data_arr['id']);//убираем id
+                        $pre_query .= $name . '=' . $val . ',';
 
-                        $upd_query = '';
-
-                        foreach ($prepare_data_arr as $name => $val) {
-
-                            $upd_query .= $name . '=' . $val . ',';
-
-                        }
-                        $upd_query = substr($upd_query,0,-1);
-
-                        $upd_query = "UPDATE $section SET $upd_query WHERE id = $section_item[id]";
-                        DB::fast_query($upd_query);
-
-
-                    } else {
-
-                        $names = implode(' , ', array_keys($prepare_data_arr)); //имена полей
-                        $vals = implode(' , ', array_values($prepare_data_arr)); // их значения
-
-                        $ins_query = "INSERT INTO $section ($names) VALUES ($vals)";
-                        DB::fast_query($ins_query);
                     }
+
+                    $pre_query = substr($pre_query, 0, -1);
+
+                    $names = implode(' , ', array_keys($prepare_data_arr)); //имена полей
+                    $vals = implode(' , ', array_values($prepare_data_arr)); // их значения
+
+                    $res_query = "INSERT INTO `$section` ($names) VALUES ($vals) ON DUPLICATE KEY UPDATE $pre_query";
+
+                    DB::fast_query($res_query);
 
                 }
 
                 $status[] = [$section => 'Обновлено успешно'];
+
             } else {
 
                 $status[] = [$section => 'Ошибка обновления-code:' . $data['info']['http_code']];
@@ -114,6 +115,82 @@ class Connector
         }
 
         return $status;
+
+    }
+
+
+    //обновление контента новостей
+    private static function reload_news_content()
+    {
+        //для результатов обновления
+        $status = [];
+
+        //получаем все id новостей из БД
+        $ids_news = DB::fast_query("SELECT `id` FROM `news`");
+
+        foreach ($ids_news as $id_news) {
+
+            $data = self::curl_get_contents(self::$connect_url . 'news/' . $id_news['id']);
+
+            if ($data['output'] && $data['info']['http_code'] == '200') {
+                $news_content = json_decode($data['output'], true)['data']['content'];
+
+                DB::fast_query("INSERT INTO `news_content` (`news_id`,`content`) VALUES ($id_news[id],'$news_content') ON DUPLICATE KEY UPDATE content='$news_content'");
+
+                $status[] = ['id новости - ' . $id_news['id'] => 'Обновлено успешно'];
+
+            } else {
+                $status[] = ['id новости - ' . $id_news['id'] => 'Ошибка обновления-code:' . $data['info']['http_code']];
+            }
+
+        }
+
+        return $status;
+
+    }
+
+    //обновление фотографий
+    private static function reload_photo()
+    {
+        //для результатов обновления
+        $status = [];
+
+        //получаем все id альбомов из БД
+        $ids_albums = DB::fast_query("SELECT `id` FROM `photo-albums`");
+
+        foreach ($ids_albums as $id_album) {
+
+            $data = self::curl_get_contents(self::$connect_url . 'photo-albums/' . $id_album['id']);
+
+            if ($data['output'] && $data['info']['http_code'] == '200') {
+                $photo_content = json_decode($data['output'], true)['data']['photos'];
+
+                foreach ($photo_content as $photo) {
+
+                    DB::fast_query("INSERT INTO `photos` (`id`,`image`,`thumb`,`created_at`,`albums_id`) VALUES ('$photo[id]','$photo[image]','$photo[thumb]','$photo[created_at]','$id_album[id]') ON DUPLICATE KEY UPDATE `image`='$photo[image]',`thumb`='$photo[thumb]',`created_at`='$photo[created_at]'");
+
+                }
+
+                $status[] = ['id альбома - ' . $id_album['id'] => 'Обновлено успешно'];
+
+            } else {
+
+                $status[] = ['id альбома - ' . $id_album['id'] => 'Ошибка обновления-code:' . $data['info']['http_code']];
+
+            }
+
+        }
+
+        return $status;
+
+
+    }
+
+
+    //получение контактных данных
+    public static function show_contacts(){
+
+        return (self::curl_get_contents(self::$connect_url.'contacts')['output']);
 
     }
 
